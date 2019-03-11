ddb5c6f5a2d22220da28314e3f8cffadc60fe209
Mr.huang <2316694914@qq.com>
2019-03-11 11:49:20 +0800

    添加基础学习代码

---
+++ 基础/learning.py
@@ -0,0 +1,23 @@
+#中文编码
+#!usr/bin/python
+# -*- coding: utf-8 -*-
+
+#单行注释
+
+"""
+多行注释
+多行注释
+"""
+#遍历
+lassmates = ['michael', 'bob', 'tracy']
+length = len(classmates)
+classmates.append('adom')
+classmates.pop()
+classmates.insert()
+#判断语句
+s = input('birthDate: ')
+birthDate = int(s)
+if birthDate < 2000:
+    print('00a黄清')
+else:
+    print('0012312312')
